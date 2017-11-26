5fdd45fc940f2878d50fb284c63b85d2af708b9b
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2017-11-27 02:45:01 +0900

    bugfix

--- Sources/Mergeable.h
+++ Sources/Mergeable.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 tattn. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Mergeable.
 FOUNDATION_EXPORT double MergeableVersionNumber;
